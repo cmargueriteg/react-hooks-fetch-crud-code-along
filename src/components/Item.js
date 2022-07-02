a3ea3de2d8da7b47e05bdbe70d12730f4be7deb5
Cindy Germain <cindymarguerite@gmail.com>
2022-07-02 14:58:37 -0600

    commit tree 238d41a9c528a6d6a51f406ded34fe8ffe493590

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,53 @@
 import React from "react";
 
-function Item({ item }) {
-  return (
-    <li className={item.isInCart ? "in-cart" : ""}>
-      <span>{item.name}</span>
-      <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
-        {item.isInCart ? "Remove From" : "Add to"} Cart
+// Destructure the onUpdateItem prop
+
+function Item({ item, onUpdateItem, onDeleteItem }) {
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => onDeleteItem(item));
+  }
+
+  function handleAddToCartClick() {
+    // Call onUpdateItem, passing the data returned from the fetch request
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        isInCart: !item.isInCart,
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedItem) => onUpdateItem(updatedItem));
+    console.log("clicked item:", item);
+
+  }
+
+
+
+
+      return (
+        <li className={item.isInCart ? "in-cart" : ""}>
+          <span>{item.name}</span>
+          <span className="category">{item.category}</span>
+          {/* add the onClick listener */}
+          <button
+            className={item.isInCart ? "remove" : "add"}
+            onClick={handleAddToCartClick}
+          >
+            {item.isInCart ? "Remove From" : "Add to"} Cart
+          </button>
+          <button className="remove" onClick={handleDeleteClick}>
+        Delete
       </button>
-      <button className="remove">Delete</button>
-    </li>
-  );
+        </li>
+      );
 }
 
 export default Item;
